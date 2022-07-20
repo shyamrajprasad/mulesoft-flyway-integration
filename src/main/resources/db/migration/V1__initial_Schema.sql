7b16618db63e2d47c95e107fc16aba33050da24c
shyam raj <shyamrajprasad@gmail.com>
2022-07-20 10:23:38 +0530

    initial commit

---
+++ src/main/resources/db/migration/V1__initial_Schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE accounts (
+ user_id serial PRIMARY KEY,
+ username VARCHAR ( 50 ) UNIQUE NOT NULL,
+ password VARCHAR ( 50 ) NOT NULL,
+ email VARCHAR ( 255 ) UNIQUE NOT NULL,
+ created_on TIMESTAMP NOT NULL,
+        last_login TIMESTAMP 
+);
+
+INSERT INTO public.accounts
+(username, "password", email,created_on, last_login)
+VALUES('mule', 'max', 'mule@liquibase.com', now(), now());
